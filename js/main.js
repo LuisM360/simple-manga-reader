30e25f1668d58c96430ec8d8b644d08cc928879a
Luis Malagon <luismalagon123@gmail.com>
2022-08-04 20:04:31 -0400

    added a new function that gets a chapter list from the api

--- js/main.js
+++ js/main.js
@@ -31,6 +31,46 @@ function getManga(){
             })
 
         })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
 
-function getChapterList
+function getChapterList(){
+    // gets the id of the li selected
+    mangaID = this.id
+
+    //clears the list element
+    document.querySelector('#listOfManga').textContent = ''
+
+    //sets the url for the fetch
+    let url = `https://api.mangadex.org/manga/${mangaID}/feed?limit=200&translatedLanguage[]=en&order%5Bchapter%5D=asc`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            
+            // grabs list of chapters from api and puts each chapter into a li
+            data.data.forEach( obj => {
+                let li = document.createElement('li')
+                li.textContent = obj.attributes.chapter
+                document.querySelector('#listOfManga').appendChild(li)
+                li.id = obj.id
+            })
+
+            // adds event listener to all of the li
+            let arrayOfLi = document.querySelectorAll('li')
+            arrayOfLi.forEach(item => {
+                item.addEventListener('click', getChapterList)
+            })
+
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
+
+function getChapterPage(){
+    
+}
